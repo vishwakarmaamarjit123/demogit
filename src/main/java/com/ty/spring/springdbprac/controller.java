c0f5ee1d02350f76c9dabad72f752408401086a4
vishwakarmaamarjit123 <amarjeetvishwakarma0352@gmail.com>
2024-10-13 20:30:23 +0530

    second commit

--- src/main/java/com/ty/spring/springdbprac/controller.java
+++ src/main/java/com/ty/spring/springdbprac/controller.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/student")
@@ -12,22 +13,30 @@ public class controller {
     private StudentRepo studentRepo;
 
     @PostMapping
-    public String save(@RequestBody Student student){
-    studentRepo.save(student);
-    return student.toString();
+    public String save(@RequestBody Student student) {
+        studentRepo.save(student);
+        return student.toString();
     }
 
     @PutMapping
-    public String update(@RequestBody Student student){
+    public String update(@RequestBody Student student) {
         studentRepo.save(student);
         return student.toString();
     }
 
     @GetMapping
-    public List<Student> find(@RequestParam double d){
-       return studentRepo.findByPrice(d);
+    public List<Student> find(@RequestParam double d) {
+        return studentRepo.findByPrice(d);
     }
-    
 
+    @DeleteMapping
+    public String deleteById(@RequestParam int id) {
+        Optional<Student> optional = studentRepo.findById(id);
+        if (optional != null) {
+            studentRepo.deleteById(id);
+            return optional.get().toString();
+        }
+        return "can't delete";
+    }
 
 }
